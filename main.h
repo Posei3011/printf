9f11410c5dff03ff14682642d3d4b5733162bd4f
Posei3011 <posei3011@gmail.com>
2023-05-30 07:51:31 -0700

    Update

--- main.h
+++ main.h
@@ -7,6 +7,14 @@
 #include <limits.h>
 #include <unistd.h>
 
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+#define S_LONG 2
+#define S_SHORT 1
 
 
 /**
